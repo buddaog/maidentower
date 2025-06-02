1b70e908b35bb6632e1b12c99088c9688d011ca3
buddaog <88316545+buddaog@users.noreply.github.com>
2025-06-03 01:23:47 +0400

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,10 @@
-
 import React from 'react';
 
 const HomePage = () => {
   return (
-    <div className="home-page">
+    <div className="p-4">
       <h1>Добро пожаловать в Maidentower Tours</h1>
-      <p>Выберите направление, узнайте подробности и отправляйтесь в приключение!</p>
+      <p>Изучите наши туры и забронируйте своё приключение!</p>
     </div>
   );
 };
