f1f26f70d18b309aedf1298328b760622fc018be
buddaog <88316545+buddaog@users.noreply.github.com>
2025-06-03 01:09:21 +0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './context/LanguageContext';
+import { CurrencyProvider } from './context/CurrencyContext';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import HomePage from './pages/HomePage';
+import ToursPage from './pages/ToursPage';
+import TourDetailPage from './pages/TourDetailPage';
+import BookingPage from './pages/BookingPage';
+
+// Import i18n
+import './i18n';
+
+function App() {
+  return (
+    <LanguageProvider>
+      <CurrencyProvider>
+        <Router>
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/tours" element={<ToursPage />} />
+                <Route path="/tours/:id" element={<TourDetailPage />} />
+                <Route path="/booking/:id" element={<BookingPage />} />
+                {/* Fallback route */}
+                <Route path="*" element={<HomePage />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </Router>
+      </CurrencyProvider>
+    </LanguageProvider>
+  );
+}
+
+export default App;
