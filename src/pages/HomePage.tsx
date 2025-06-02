b063a5814dc26358a7c132dc71a80481a3fbcb9a
buddaog <88316545+buddaog@users.noreply.github.com>
2025-06-03 01:19:29 +0400

    Add files via upload

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Добро пожаловать в Maidentower Tours</h1>
+      <p>Выберите направление, узнайте подробности и отправляйтесь в приключение!</p>
+    </div>
+  );
+};
+
+export default HomePage;
